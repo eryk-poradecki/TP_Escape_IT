818ac2315562c45531baa6c418fa7cfaf99793f0
eryk-poradecki <eryk.poradecki@gmail.com>
2023-05-30 21:45:23 +0200

    feat: added initial TTS implementation using a local file

--- Escape_IT/Escape_IT_Gamemaster/views.py
+++ Escape_IT/Escape_IT_Gamemaster/views.py
@@ -1,8 +1,39 @@
+import sys
+import os
+
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from django.http import FileResponse
 from django.shortcuts import render
 
-def home(request):
 
-	context = {
-		'title': 'Gamemaster home'
-	}
-	return render(request, 'Escape_IT_Gamemaster/home.html', context)
+def home(request):
+    context = {
+        'title': 'Gamemaster home'
+    }
+
+    if request.method == 'POST':
+        text = request.POST.get('text_to_convert')
+        if text:
+            session = Session(profile_name="default")
+            polly_client = session.client('polly')
+
+            try:
+                response = polly_client.synthesize_speech(
+                    Text=text,
+                    VoiceId='Brian',
+                    OutputFormat='mp3')
+            except(BotoCoreError, ClientError) as error:
+                print(error)
+                sys.exit(-1)
+
+            file_path = 'E:/Unity/Projects/Escape_IT/Assets/Sound effects/speech.mp3'
+
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+            with open(file_path, 'wb') as file:
+                file.write(response['AudioStream'].read())
+
+
+    return render(request, 'Escape_IT_Gamemaster/home.html', context)
