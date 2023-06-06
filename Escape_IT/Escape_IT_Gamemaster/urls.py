dedf0fc98bdf48bad49110d6bef0fc5efcd553ad
eryk-poradecki <eryk.poradecki@gmail.com>
2023-06-06 20:25:08 +0200

    refactor: replacing main branch, moving standard django files

--- Escape_IT/Escape_IT_Gamemaster/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views as gamemaster_views
-
-urlpatterns = [
-	path('', gamemaster_views.home, name='gamemaster_home'),
-]
--- Escape_IT/game_master/apps.py
+++ Escape_IT/game_master/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EscapeItGamemasterConfig(AppConfig):
+class GameMasterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Escape_IT_Gamemaster'
+    name = 'game_master'
