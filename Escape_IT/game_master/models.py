f7954f82244381b20a98f0bbb9f2811c8bded20d
eryk-poradecki <eryk.poradecki@gmail.com>
2023-06-10 19:58:05 +0200

    chore: PEP8 blank lines correction

--- Escape_IT/game_master/models.py
+++ Escape_IT/game_master/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Room(models.Model):
     room_number = models.IntegerField()
     room_name = models.CharField(max_length=200)
     room_description = models.CharField(max_length=200)
+
 
 class Game(models.Model):
     active = models.BooleanField()
