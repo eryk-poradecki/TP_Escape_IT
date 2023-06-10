2abe627c49c29d0095c26bb5f88f7c1c29202747
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-10 18:24:05 +0200

    Added Game model

--- Escape_IT/game_master/models.py
+++ Escape_IT/game_master/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    active = models.BooleanField()
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    progress = models.IntegerField()
+    room_number = models.IntegerField()
