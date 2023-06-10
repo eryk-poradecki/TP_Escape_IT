8157fac5476116ccf3dd3efe8785ab979bd2a703
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-10 18:38:45 +0200

    Added room model

--- Escape_IT/game_master/models.py
+++ Escape_IT/game_master/models.py
@@ -7,4 +7,9 @@ class Game(models.Model):
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
     progress = models.IntegerField()
+
+class Room(models.Model):
     room_number = models.IntegerField()
+    room_name = models.CharField(max_length=200)
+    room_description = models.CharField(max_length=200)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
