81e5ffc168547a71a9c89743a91e71bd61479513
eryk-poradecki <eryk.poradecki@gmail.com>
2023-06-10 20:01:56 +0200

    feat: added general and detailed views for rooms

--- Escape_IT/game_master/views.py
+++ Escape_IT/game_master/views.py
@@ -1,45 +1,34 @@
-from .models import Rooms
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
+from .models import Room, Game
 
-# Create your views here.
 
 def home(request):
     return render(request, 'game_master/home.html')
 
-def notifications(request): 
+
+def notifications(request):
     return render(request, 'game_master/notifications.html')
+
 
 def settings(request):
     return render(request, 'game_master/settings.html')
 
-def get_room_data(request):
 
-    rooms_data = {
-        'room_number': 1,
-        'room_name': 'Room 1',
-        'room_description': 'This is room 1',
-    } #for all the records 
-    template = loader.get_template('base.html')
-    context={
-      'rooms_data': rooms_data,    
-    } 
+def rooms(request):
+    context = {
+        'rooms': Room.objects.all()
+    }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'game_master/rooms.html', context)
 
-def get_games_data(request):
 
-    games_data = {
-        'active': True,
-        'start_date_time': '2021-10-10 10:10:10',
-        'end_date_time': '2021-10-10 10:10:10',
-        'progress': 1,
-        'room': 1,
-    } #for all the records 
-    template = loader.get_template('base.html')
-    context={
-      'games_data': games_data,    
-    } 
+def room_detail(request, room_id):
+    room = get_object_or_404(Room, id=room_id)
 
-    return HttpResponse(template.render(context, request))
+    context = {
+        'room': room
+    }
+
+    return render(request, 'game_master/room-detail.html', context)
