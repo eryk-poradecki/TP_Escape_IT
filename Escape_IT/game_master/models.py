76821612f73e8888d9d8d81f2f3533e06df5617c
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-10 18:52:22 +0200

    fixed model

--- Escape_IT/game_master/models.py
+++ Escape_IT/game_master/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Room(models.Model):
+    room_number = models.IntegerField()
+    room_name = models.CharField(max_length=200)
+    room_description = models.CharField(max_length=200)
+
 class Game(models.Model):
     active = models.BooleanField()
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
     progress = models.IntegerField()
-
-class Room(models.Model):
-    room_number = models.IntegerField()
-    room_name = models.CharField(max_length=200)
-    room_description = models.CharField(max_length=200)
-    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
