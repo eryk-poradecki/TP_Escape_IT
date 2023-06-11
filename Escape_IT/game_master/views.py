e5c7c9bd5cccd9173f3e6155fcba41fb283d3d65
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-11 16:49:26 +0200

    Added dynamic games display

--- Escape_IT/game_master/views.py
+++ Escape_IT/game_master/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 from .models import Room, Game
+from datetime import datetime
 
 
 def home(request):
@@ -19,7 +20,12 @@ def settings(request):
 def room_panel(request, room_id):
     room = get_object_or_404(Room, id=room_id)
     context = {
-        'room': room
+        'room': room,
+        'games': Game.objects.filter(room=room_id),
+        'current_game': Game.objects
+            .filter(room=room_id)
+            .filter(start_date_time__lte=datetime.now())
+            .filter(end_date_time__gte=datetime.now())[0]
     }
     return render(request, 'game_master/room-panel-view.html', context)
 
