ca21bb4e77a7c2a2b274caf56fe809bcd7f2d3d4
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-12 17:14:57 +0200

    Fixed room panel screen view with no active game

--- Escape_IT/game_master/views.py
+++ Escape_IT/game_master/views.py
@@ -25,7 +25,7 @@ def room_panel(request, room_id):
         'current_game': Game.objects
             .filter(room=room_id)
             .filter(start_date_time__lte=datetime.now())
-            .filter(end_date_time__gte=datetime.now())[0]
+            .filter(end_date_time__gte=datetime.now())
     }
     return render(request, 'game_master/room-panel-view.html', context)
 
