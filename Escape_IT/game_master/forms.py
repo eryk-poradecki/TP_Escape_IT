bad89af42230c84b9a4a9dd5043af99bba1742f7
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-18 09:02:54 +0200

    Fixed datetime field

--- Escape_IT/game_master/forms.py
+++ Escape_IT/game_master/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Game
-from django.forms import Textarea
+from django.forms import Textarea, DateTimeField
 
 
 class CreateGameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ['start_date_time', 'end_date_time', 'progress']
+        widgets = {
+            'start_date_time': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'end_date_time': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
         
