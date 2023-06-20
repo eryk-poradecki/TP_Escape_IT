a1837885ea9eeb21b46d90cd1a9eb17680dfb397
MateuszPryl <mateusz.pryl2@gmail.com>
2023-06-20 09:11:09 +0200

    Added dynamic notification display

--- Escape_IT/game_master/consumers.py
+++ Escape_IT/game_master/consumers.py
@@ -83,11 +83,11 @@ class UnityConsumer(WebsocketConsumer):
         type = text_data_json['type']
 
         if type == 'help_request':
-            notification_message = f"Players in room {self.room_id} need help!"
+            notification_message = f"Players in Room {self.room_id} need help!"
             Notification.objects.create(
                 type=type,
                 message=notification_message,
-                date_time=timezone.now(),
+                date_time=timezone.now().__add__(timezone.timedelta(hours=2)),
                 room=Room.objects.filter(id=self.room_id).first(),
             )
 
@@ -100,11 +100,11 @@ class UnityConsumer(WebsocketConsumer):
                 }
             )
         elif type =='custom_help_request':
-            notification_message = text_data_json['message']
+            notification_message = f"Room {self.room_id} " + text_data_json['message']
             Notification.objects.create(
                 type=type,
                 message=notification_message,
-                date_time=timezone.now(),
+                date_time=timezone.now().__add__(timezone.timedelta(hours=2)),
                 room=Room.objects.filter(id=self.room_id).first(),
             )
 
@@ -117,30 +117,33 @@ class UnityConsumer(WebsocketConsumer):
                 }
             )
         elif type == 'progress':
-            current_date_time = timezone.now()
-            game = Game.objects.filter(
-                room_id=self.room_id,
-                start_date_time__lte=current_date_time,
-                end_date_time__gte=current_date_time,
-            ).latest('start_date_time')
-            game.progress += 25
-            game.save()
-            notification_message = f'Players in room {self.room_id} made progress. Current progress: {game.progress}%'
-            Notification.objects.create(
-                type=type,
-                message=notification_message,
-                date_time=current_date_time,
-                room=Room.objects.filter(id=self.room_id).first(),
-            )
-            async_to_sync(self.channel_layer.group_send)(
-                'web',
-                {
-                    'type': 'event_handler',
-                    'event_type': type,
-                    'message': notification_message
-                }
-            )
-
+            current_date_time = timezone.now().__add__(timezone.timedelta(hours=2))
+            try:
+                game = Game.objects.filter(
+                    room_id=self.room_id,
+                    start_date_time__lte=current_date_time,
+                    end_date_time__gte=current_date_time,
+                ).latest('start_date_time')
+                game.progress += 25
+                game.save()
+                notification_message = f'Players in Room {self.room_id} made progress. Current progress: {game.progress}%'
+                Notification.objects.create(
+                    type=type,
+                    message=notification_message,
+                    date_time=current_date_time,
+                    room=Room.objects.filter(id=self.room_id).first(),
+                )
+                async_to_sync(self.channel_layer.group_send)(
+                    'web',
+                    {
+                        'type': 'event_handler',
+                        'event_type': type,
+                        'message': notification_message
+                    }
+                )
+            except (Game.DoesNotExist):
+                print("Game does not exist")
+            
 
     def disconnect(self, code):
         self.channel_layer.group_discard(
--- Escape_IT/game_master/static/scripts/notifications_script.js
+++ Escape_IT/game_master/static/scripts/notifications_script.js
@@ -1,25 +1,34 @@
 
-console.log("chuj")
 
 let url = `ws://${window.location.host}/ws/socket-server/web`
 
 const chatSocket = new WebSocket(url);
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 chatSocket.onmessage = function(e){
     let data = JSON.parse(e.data)
     
     console.log(data)
 
-    if (data.type == 'help_request') {
-        console.log("help request")
+    if (data.type == "help_request" || data.type == "custom_help_request" || data.type == "progress") {
+        let div = document.createElement('div');
+        div.setAttribute("class", "notification");
 
-        // Tworzenie elementu ktory bedzie wrzucony do listy notyfikacji
-        // TODO: Dopasowac do elementu i klass z css
-        var p = document.createElement('p');
-        p.setAttribute("class", "notification");
-        p.innerText = data.message;
-        notificationContainer.appendChild(p);
+        let p = document.createElement('p');
+        p.innerText = data.message + ". Click here to send help!";
+
+        let pDate = document.createElement('p');
+        let date = new Date()
+        let minutesDisplay = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+        pDate.innerText = months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() + ", " + date.getHours() + ":" + minutesDisplay;
+
+        div.appendChild(p);
+        div.appendChild(pDate);
+
+        notificationContainer.appendChild(div);
     }
+    
 }
 
 let notificationContainer = null
